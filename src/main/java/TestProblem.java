746c1dd3ca42cee2deb0d51e0abb14c6d26ef7bf
Nazim <108560858+NazimTheAllFather@users.noreply.github.com>
2025-08-17 00:32:56 +0000

    hhhh

---
+++ src/main/java/TestProblem.java
@@ -0,0 +1,10 @@
+class testProblem{
+    public static void main(String[] args){
+        System.out.println("Hello world!");
+        System.out.println("Hello world2!");
+    }
+
+    public void someMethod(){
+        System.out.println("Hello world!3");
+    }
+}
